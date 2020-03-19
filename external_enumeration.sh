f72ea664e1f80e8da7a5139b52a3d8e0281d52e9
Connor McGarr <45445720+connormcgarr@users.noreply.github.com>
2020-03-19 11:09:33 -0500

    Update external_enumeration.sh

--- external_enumeration.sh
+++ external_enumeration.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # Script to automate some initial enumeration for external assessments
-# Author: Connor McGarr
+# Author: Connor McGarr (@33y0re)
 
 # Domain name variable
 domain=$1
+
+# Extracting just domain for organizational SSL search in Shodan
+domain_no_com=$(echo $domain | awk -F '.' '{print $1}')
 
 # Shodan API key variable
 shodan_api=$2
@@ -89,7 +92,6 @@ echo "[+] All DNS records (via dig) for $domain have been saved in DNS_Records_$
 echo "[+] A cleaner version of dig DNS records for $domain is located in Cleaned_Up_DNS_Records_$domain.txt"
 
 # Shodan function
-# Shodan function
 function run_shodan
 {
 
@@ -99,17 +101,29 @@ function run_shodan
 	mkdir $PWD/INITIAL_EXTERNAL_ENUMERATION/Shodan_Output
 
 	# Any easy wins?
+	echo "[+] Downloading Shodan information for any easy wins. Please wait- this may take some time..."
 	shodan download search "port:21,23,139,445,3389,5900" $domain >/dev/null
 	shodan parse --fields ip_str,port,org --separator , search.json.gz >> $PWD/INITIAL_EXTERNAL_ENUMERATION/Shodan_Output/21_23_139_445_3389_5900_SHODAN_OUTPUT.csv
 
 	# Web servers?
+	echo "[+] Downloading Shodan information for any potential web servers. Please wait- this may take some time..."
 	shodan download search "port:80,443,8080,8443" $domain >/dev/null
 	shodan parse --fields ip_str,port,org --separator , search.json.gz >> $PWD/INITIAL_EXTERNAL_ENUMERATION/Shodan_Output/WEB_SERVERS_SHODAN_OUTPUT.csv
 
+	# SSL certificates notice
+	echo "[+] Please note- if the name of the organization is different than $domain_no_com- please perform a manual search for SSL certificates referencing the organization..."
+
+	# Sleep to allow user to read information above
+	sleep 5
+
+	# SSL certificates?
+	echo "[+] Downloading Shodan information for any SSL certificates referencing $domain_no_com. Please wait- this may take some time..."
+	shodan download search "ssl:*$domain_no_com" 200 >/dev/null
+	shodan parse --fields ip_str,port,org --separator , search.json.gz >> $PWD/INITIAL_EXTERNAL_ENUMERATION/Shodan_Output/SSL_CERTS_SHODAN_OUTPUT.csv
+
 	# Clean up
+	echo "[+] Cleaning up!"
 	rm $PWD/search.json.gz
 }
 
 run_shodan
-
-# TODO
